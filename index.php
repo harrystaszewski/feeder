9e1d912b97bd91361f7d104624227331a06860d6
Harry Staszewski <harry@tanktopstudios.co.uk>
2013-08-21 23:08:28 +0100

    First commit.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link href="css\feeder.css" rel="stylesheet" type="text/css" />
+        <script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+        <script>
+          WebFont.load({
+            google: {
+              families: ['Droid Serif', 'Rosario', 'Fauna One']
+            }
+          });
+        </script>
+        
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="scripts/feeder.js"></script>
+    </head>
+    <body>
+        <div id="canvas">
+            <div id="message">I am a tweet. I have exactly 140 characters.
+                If you want to make sure that I only have 140 characters you can check for yourself.
+                0123456789</div>
+        </div>
+    </body>
+</html>
---
+++ scripts/feeder.js
@@ -0,0 +1,69 @@
+$(function() {
+    setTimeout("location.reload(false);",10000);
+    randomTransition();
+});
+
+function randomFont() {
+    var fontArray = new Array('Rosario', 'Droid Serif', 'Fauna One');
+    return fontArray[Math.floor(Math.random()*fontArray.length)];
+}
+
+function randomTransition() {
+    var transition;
+    transition = Math.floor((Math.random()*4)+1);
+    $("#message").css({"font-family": randomFont() + ", serif"});
+    switch (transition) {
+        case 1:
+            fromLeft();
+            break;
+        case 2:
+            fromTop();
+            break;
+        case 3:
+            fromRight();
+            break;
+        case 4:
+            fromBottom();
+            break;
+        default:
+            fromLeft();
+    }
+}
+
+function fromRight () {
+    $("#message").css({
+        "left": "50%",
+        "top": ($(window).height() - $("#message").outerHeight())/2.4
+    }).animate({
+        left: "0%",
+        opacity: 1
+    }, 3000);
+}
+
+function fromLeft () {
+    $("#message").css({
+        "top": ($(window).height() - $("#message").outerHeight())/2.4,
+        "left": "-50%"
+    }).animate({
+        left: "0%",
+        opacity: 1
+    }, 3000);
+}
+
+function fromTop () {
+    $("#message").css({
+        "left": "0%"
+    }).animate({
+        "top": ($(window).height() - $("#message").outerHeight())/2.4,
+        opacity: 1
+    }, 3000);
+}
+
+function fromBottom() {
+    $("#message").css({
+        "top": "80%"
+    }).animate({
+        "top": ($(window).height() - $("#message").outerHeight())/2.4,
+        opacity: 1
+    }, 3000);
+}
